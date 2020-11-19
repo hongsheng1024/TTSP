e88963ee792b8e9366d0aedb2b3e6b6ba5b7cf85
王鸿升 <wanghs@dingchi.com>
2020-11-19 17:17:58 +0800

    project

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TTSP
+//
+//  Created by whs on 2020/11/19.
+//  Copyright © 2020 whs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TTSP
+//
+//  Created by whs on 2020/11/19.
+//  Copyright © 2020 whs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
