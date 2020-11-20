1ef971ca09f3547cc353ce5913454243b276a4ee
王鸿升 <wanghs@dingchi.com>
2020-11-20 09:37:54 +0800

    测试

--- TTSP/TTSP/Runtime/HSRuntimeViewController.h
+++ TTSP/TTSP/Runtime/HSRuntimeViewController.h
@@ -6,11 +6,11 @@
 //  Copyright © 2020 whs. All rights reserved.
 //
 
-#import "BaseViewController.h"
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface HSRuntimeViewController : BaseViewController
+@interface HSRuntimeViewController : UIViewController
 
 @end
 
